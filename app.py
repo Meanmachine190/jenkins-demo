ded7aff4653bce89775a611e9cdd76f2de57da6a
meanmachine190 <arshadyaalimadad@gmail.com>
2025-04-22 13:52:40 +0530

    promo commit

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from prometheus_client import start_http_server, Summary, Counter, generate_latest
+
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins!"
+    REQUEST_COUNT.inc()
+    return "Hello from Jenkins with metrics!"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 if __name__ == "__main__":
+    # Start Prometheus client on default port 8000
+    start_http_server(8000)
     app.run(host='0.0.0.0', port=5000)
+
